c8327c1dbb7d93113bfb379e92a4c49213f67614
David Hunter <dph@alumni.neu.edu>
2017-12-12 07:49:06 -0800

    Added simplier way to obtain Nest information

--- nest_simple.py
+++ nest_simple.py
@@ -1,26 +1,29 @@
 #! /usr/bin/python
 
-# nest.py -- a python interface to the Nest Thermostat
-# by Scott M Baker, smbaker@gmail.com, http://www.smbaker.com/
+# nest-watch.py -- logs Nest Thermostat and observed weather
+#                  information to an influxdb
+#
+# by David Hunter, dph@alumni.neu.edu 
 #
 # Usage:
-#    'nest.py help' will tell you what to do and how to do it
-#
-# Licensing:
-#    This is distributed unider the Creative Commons 3.0 Non-commecrial,
-#    Attribution, Share-Alike license. You can use the code for noncommercial
-#    purposes. You may NOT sell it. If you do use it, then you must make an
-#    attribution to me (i.e. Include my name and thank me for the hours I spent
-#    on this)
+#    Reads in a configuration file (nest-watch.ini).  By default it reads the
+#    first thermostat found in a user account.  Use the device_index to specify
+#    another one. 
 #
 # Acknowledgements:
-#    Chris Burris's Siri Nest Proxy was very helpful to learn the nest's
-#       authentication and some bits of the protocol.
+#    Thanks to Scott M Baker's nest.py which this is based on 
+#    for interacting with the Nest Thermostat
 
 import urllib
 import urllib2
 import sys
-from optparse import OptionParser
+import ConfigParser
+from ConfigParser import SafeConfigParser
+import pyowm
+from pyowm.utils import temputils
+import influxdb
+import os
+import os.path
 
 try:
    import json
@@ -31,6 +34,22 @@ except ImportError:
        print "No json library available. I recommend installing either python-json"
        print "or simpejson."
        sys.exit(-1)
+
+class WeatherObj(object):
+    def __init__(self, data):
+	self.__dict__ = json.loads(data)
+
+class Configuration(object):
+    def __init__(self, config_file):
+        if os.path.isfile(config_file) and os.access(config_file, os.R_OK):
+            print "config file found and is readable"
+        else:
+            print "Either file is missing or not readable"
+
+    def read(self):
+        NestConfig = SafeConfigParser()
+        NestConfig.read(self.config_file)
+        return NestConfig
 
 class Nest:
     def __init__(self, username, password, serial=None, index=0, units="F"):
@@ -68,24 +87,19 @@ class Nest:
                                        "Authorization":"Basic " + self.access_token,
                                        "X-nl-user-id": self.userid,
                                        "X-nl-protocol-version": "1"})
-
         res = urllib2.urlopen(req).read()
-
         res = self.loads(res)
-
         self.structure_id = res["structure"].keys()[0]
+        print self.structure_id
 
         if (self.serial is None):
             self.device_id = res["structure"][self.structure_id]["devices"][self.index]
             self.serial = self.device_id.split(".")[1]
-
         self.status = res
+        print json.dumps(res, indent=4, sort_keys=True)
+#        print res
 
-        #print "res.keys", res.keys()
-        #print "res[structure][structure_id].keys", res["structure"][self.structure_id].keys()
-        #print "res[device].keys", res["device"].keys()
-        #print "res[device][serial].keys", res["device"][self.serial].keys()
-        #print "res[shared][serial].keys", res["shared"][self.serial].keys()
+#    def get_info(self):
 
     def temp_in(self, temp):
         if (self.units == "F"):
@@ -102,131 +116,52 @@ class Nest:
     def show_status(self):
         shared = self.status["shared"][self.serial]
         device = self.status["device"][self.serial]
-
         allvars = shared
         allvars.update(device)
-
         for k in sorted(allvars.keys()):
              print k + "."*(32-len(k)) + ":", allvars[k]
 
     def show_curtemp(self):
         temp = self.status["shared"][self.serial]["current_temperature"]
         temp = self.temp_out(temp)
-
         print "%0.1f" % temp
 
-    def set_temperature(self, temp):
-        temp = self.temp_in(temp)
-
-        data = '{"target_change_pending":true,"target_temperature":' + '%0.1f' % temp + '}'
-        req = urllib2.Request(self.transport_url + "/v2/put/shared." + self.serial,
-                              data,
-                              {"user-agent":"Nest/1.1.0.10 CFNetwork/548.0.4",
-                               "Authorization":"Basic " + self.access_token,
-                               "X-nl-protocol-version": "1"})
-
-        res = urllib2.urlopen(req).read()
-
-        print res
-
-    def set_fan(self, state):
-        data = '{"fan_mode":"' + str(state) + '"}'
-        req = urllib2.Request(self.transport_url + "/v2/put/device." + self.serial,
-                              data,
-                              {"user-agent":"Nest/1.1.0.10 CFNetwork/548.0.4",
-                               "Authorization":"Basic " + self.access_token,
-                               "X-nl-protocol-version": "1"})
-
-        res = urllib2.urlopen(req).read()
-
-        print res
-
-def create_parser():
-   parser = OptionParser(usage="nest [options] command [command_options] [command_args]",
-        description="Commands: fan temp",
-        version="unknown")
-
-   parser.add_option("-u", "--user", dest="user",
-                     help="username for nest.com", metavar="USER", default=None)
-
-   parser.add_option("-p", "--password", dest="password",
-                     help="password for nest.com", metavar="PASSWORD", default=None)
-
-   parser.add_option("-c", "--celsius", dest="celsius", action="store_true", default=False,
-                     help="use celsius instead of farenheit")
-
-   parser.add_option("-s", "--serial", dest="serial", default=None,
-                     help="optional, specify serial number of nest thermostat to talk to")
-
-   parser.add_option("-i", "--index", dest="index", default=0, type="int",
-                     help="optional, specify index number of nest to talk to")
-
-
-   return parser
-
-def help():
-    print "syntax: nest [options] command [command_args]"
-    print "options:"
-    print "   --user <username>      ... username on nest.com"
-    print "   --password <password>  ... password on nest.com"
-    print "   --celsius              ... use celsius (the default is farenheit)"
-    print "   --serial <number>      ... optional, specify serial number of nest to use"
-    print "   --index <number>       ... optional, 0-based index of nest"
-    print "                                (use --serial or --index, but not both)"
-    print
-    print "commands: temp, fan, show, curtemp, curhumid"
-    print "    temp <temperature>    ... set target temperature"
-    print "    fan [auto|on]         ... set fan state"
-    print "    show                  ... show everything"
-    print "    curtemp               ... print current temperature"
-    print "    curhumid              ... print current humidity"
-    print
-    print "examples:"
-    print "    nest.py --user joe@user.com --password swordfish temp 73"
-    print "    nest.py --user joe@user.com --password swordfish fan auto"
 
 def main():
-    parser = create_parser()
-    (opts, args) = parser.parse_args()
+    Configuration("./nest_track.ini")
+    NestConfig = SafeConfigParser()
+    NestConfig.read("./nest_track.ini")
 
-    if (len(args)==0) or (args[0]=="help"):
-        help()
-        sys.exit(-1)
+    n_id = NestConfig.get('Thermostat', 'username')
+    n_secret = NestConfig.get('Thermostat', 'password')
+    n_cache_file = NestConfig.get('Thermostat', 'cache_file')
 
-    if (not opts.user) or (not opts.password):
-        print "how about specifying a --user and --password option next time?"
-        sys.exit(-1)
+    db_server = NestConfig.get('Database', 'server')
+    db_port = NestConfig.get('Database', 'port')
+    db_user = NestConfig.get('Database', 'user')
+    db_pwd = NestConfig.get('Database', 'pwd')
+    db_name = NestConfig.get('Database', 'name')
 
-    if opts.celsius:
-        units = "C"
-    else:
-        units = "F"
+    w_api_key = NestConfig.get('Weather', 'api_key')
+    w_location = NestConfig.get('Weather', 'location')
+    n_lat = NestConfig.get('Weather', 'lat')
+    n_long = NestConfig.get('Weather', 'long')
 
-    n = Nest(opts.user, opts.password, opts.serial, opts.index, units=units)
+
+    print "..."
+
+#
+#    if opts.celsius:
+#        units = "C"
+#    else:
+#        units = "F"
+
+    n = Nest(n_id, n_secret)
     n.login()
     n.get_status()
-
-    cmd = args[0]
-
-    if (cmd == "temp"):
-        if len(args)<2:
-            print "please specify a temperature"
-            sys.exit(-1)
-        n.set_temperature(int(args[1]))
-    elif (cmd == "fan"):
-        if len(args)<2:
-            print "please specify a fan state of 'on' or 'auto'"
-            sys.exit(-1)
-        n.set_fan(args[1])
-    elif (cmd == "show"):
-        n.show_status()
-    elif (cmd == "curtemp"):
-        n.show_curtemp()
-    elif (cmd == "curhumid"):
-        print n.status["device"][n.serial]["current_humidity"]
-    else:
-        print "misunderstood command:", cmd
-        print "do 'nest.py help' for help"
+    n.show_status()
+    n.show_curtemp()
+    print n.status["device"][n.serial]["current_humidity"]
 
 if __name__=="__main__":
    main()
